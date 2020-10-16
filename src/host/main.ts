83c031422e8ef22c0a4472b6a302116713081db0
Maik Riechert <maik.riechert@arcor.de>
2020-10-16 20:42:00 +0100

    add rom selector

--- src/host/main.ts
+++ src/host/main.ts
@@ -1,5 +1,5 @@
 import { AsBind } from "as-bind"
-import { getRoms, defaultRom } from "./roms"
+import { getRoms, defaultRom, Roms } from "./roms"
 
 // Map key codes to CHIP-8 keys.
 // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code/code_values
@@ -33,6 +33,18 @@ function hex(n: number) {
     return '0x' + n.toString(16)
 }
 
+let roms: Roms
+let exports: any
+let unboundExports: any
+let timer = 0
+let log: boolean
+
+let canvas: HTMLCanvasElement
+let ctx: CanvasRenderingContext2D
+
+const offColor = [0, 0, 0]
+const onColor = [255, 255, 255]
+
 async function main() {
     const imports = {
     }
@@ -45,71 +57,33 @@ async function main() {
     // All returned typed arrays then become {ptr: wasm_ptr, value: arr}.
     module.enableExportFunctionUnsafeReturnValue()
 
-    const exports = module.exports
-    const unboundExports = module.unboundExports
+    exports = module.exports
+    unboundExports = module.unboundExports
     
-    const roms = await getRoms()
-    const rom = roms[defaultRom]
-    const romResponse = await fetch(rom.romUrl)
-    const romBuf = await romResponse.arrayBuffer()
+    roms = await getRoms()
 
-    const canvas = document.getElementById('display') as HTMLCanvasElement
-    const ctx = canvas.getContext("2d")
+    const romSelector = document.getElementById('rom-selector') as HTMLSelectElement
+    for (let [romId, rom] of Object.entries(roms)) {
+        var option = document.createElement("option")
+        option.value = romId
+        option.text = rom.title
+        romSelector.add(option) 
+    }
 
-    const offColor = [0, 0, 0]
-    const onColor = [255, 255, 255]
+    const loadRomBtn = document.getElementById('load-rom-btn') as HTMLButtonElement
+    loadRomBtn.addEventListener('click', e => {
+        loadRom(romSelector.value)
+    })
+
+    canvas = document.getElementById('display') as HTMLCanvasElement
+    ctx = canvas.getContext("2d")
 
     const urlParams = new URLSearchParams(window.location.search)
-    const log = urlParams.get('log') == '1'
+    log = urlParams.get('log') == '1'
 
-    if (log)
+    if (log) {
         console.log(exports.getInstructionTypes())
-
-    exports.init()
-    exports.loadProgram(new Uint8Array(romBuf))
-    function step() {
-        const raster: Uint8Array = exports.getDisplayRaster().value
-        const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-        const rgba = imgData.data
-        for (let i = 0; i < raster.length; i++) {
-            const isOn = raster[i]
-            rgba[i*4 + 0] = isOn ? onColor[0] : offColor[0]
-            rgba[i*4 + 1] = isOn ? onColor[1] : offColor[1]
-            rgba[i*4 + 2] = isOn ? onColor[2] : offColor[2]
-            rgba[i*4 + 3] = 255 // opaque
-        }
-        ctx.putImageData(imgData, 0, 0)
-
-        if (log && exports.isSoundOn()) {
-            console.log('sound: on')
-        }
-
-        // u16 return values are buggy with as-bind, using direct access here.
-        // https://github.com/torch2424/as-bind/issues/50
-
-        if (log) {
-            console.log('ram', exports.getRam().value)
-            console.log('raster', raster)
-            console.log('keys', bin(unboundExports.getKeys()))
-            console.log('register V', exports.getVRegisters().value)
-            console.log('register I', hex(unboundExports.getIRegister()))
-            console.log('register DT', exports.getDTRegister())
-            console.log('register ST', exports.getSTRegister())
-            console.log('register PC', hex(unboundExports.getPCRegister()))
-            console.log('register SP', exports.getSPRegister())
-            console.log('register stack', exports.getStackRegister().value)
-            console.log(`instruction: ${exports.getInstructionTypeName()} ${exports.getInstructionParameters()}`)
-        }
-        exports.stepCPU()
     }
-
-    (<any>window).stopChip8 = false
-    function loop() {
-        step()
-        if (!(<any>window).stopChip8)
-            setTimeout(loop, 200)
-    }
-    loop()
 
     document.addEventListener('keydown', e => {
         const chip8Key = keyMap[e.code]
@@ -119,6 +93,54 @@ async function main() {
         const chip8Key = keyMap[e.code]
         exports.setKeyDown(chip8Key, false)
     })
+}
+
+function step() {
+    const raster: Uint8Array = exports.getDisplayRaster().value
+    const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    const rgba = imgData.data
+    for (let i = 0; i < raster.length; i++) {
+        const isOn = raster[i]
+        rgba[i*4 + 0] = isOn ? onColor[0] : offColor[0]
+        rgba[i*4 + 1] = isOn ? onColor[1] : offColor[1]
+        rgba[i*4 + 2] = isOn ? onColor[2] : offColor[2]
+        rgba[i*4 + 3] = 255 // opaque
+    }
+    ctx.putImageData(imgData, 0, 0)
+
+    if (log && exports.isSoundOn()) {
+        console.log('sound: on')
+    }
+
+    // u16 return values are buggy with as-bind, using direct access here.
+    // https://github.com/torch2424/as-bind/issues/50
+
+    if (log) {
+        console.log('ram', exports.getRam().value)
+        console.log('raster', raster)
+        console.log('keys', bin(unboundExports.getKeys()))
+        console.log('register V', exports.getVRegisters().value)
+        console.log('register I', hex(unboundExports.getIRegister()))
+        console.log('register DT', exports.getDTRegister())
+        console.log('register ST', exports.getSTRegister())
+        console.log('register PC', hex(unboundExports.getPCRegister()))
+        console.log('register SP', exports.getSPRegister())
+        console.log('register stack', exports.getStackRegister().value)
+        console.log(`instruction: ${exports.getInstructionTypeName()} ${exports.getInstructionParameters()}`)
+    }
+    exports.stepCPU()
+}
+
+async function loadRom(romId: string) {
+    window.clearInterval(timer)
+
+    const rom = roms[romId]
+    const romResponse = await fetch(rom.romUrl)
+    const romBuf = await romResponse.arrayBuffer()
+    exports.init()
+    exports.loadProgram(new Uint8Array(romBuf))
+
+    timer = window.setInterval(step, 200)
 }
 
 main()
--- src/host/roms.ts
+++ src/host/roms.ts
@@ -1,17 +1,27 @@
 export const defaultRom = "chip8-test-rom"
 
-export async function getRoms() {
+export interface Rom {
+    title: string
+    infoUrl: string
+    romUrl: string
+    txtUrl?: string
+    platform: string
+    options: {}
+}
+
+export type Roms = { [romId: string]: Rom }
+
+export async function getRoms(): Promise<Roms> {
     let r = await fetch("https://raw.githubusercontent.com/JohnEarnest/chip8Archive/master/programs.json")
-    let j = await r.json()
-    for (const id in j) {
-        j[id].infoUrl = "https://github.com/JohnEarnest/chip8Archive/tree/master/roms/" + id + ".ch8"
-        j[id].romUrl = toDownloadUrl(j[id].infoUrl)
+    const roms: Roms = await r.json()
+    for (const id in roms) {
+        roms[id].infoUrl = "https://github.com/JohnEarnest/chip8Archive/tree/master/roms/" + id + ".ch8"
+        roms[id].romUrl = toDownloadUrl(roms[id].infoUrl)
     }
 
     r = await fetch("https://api.github.com/repos/dmatlack/chip8/git/trees/master?recursive=1")
-    j = await r.json()
-
-    for (const item of j.tree) {
+    const tree = await r.json()
+    for (const item of tree.tree) {
         if (!item.path.endsWith(".ch8"))
             continue
         const url = "https://github.com/dmatlack/chip8/tree/master/" + item.path
@@ -19,24 +29,25 @@ export async function getRoms() {
         const title = filename.replace(".ch8", "")
         let txtUrl = url.replace(' (alt)', '')
         txtUrl = txtUrl.replace('.ch8', '.txt')
-        j[title] = {
+        roms[title] = {
             title: title,
             infoUrl: url,
             romUrl: toDownloadUrl(url),
             txtUrl: toDownloadUrl(txtUrl),
             platform: "chip8",
-            options: {
-            }
-        }
+            options: {}
+        } as Rom
     }
 
-    j["chip8-test-rom"] = {
+    roms["chip8-test-rom"] = {
         title: "chip8-test-rom",
         infoUrl: "https://github.com/corax89/chip8-test-rom",
-        romUrl: toDownloadUrl("https://github.com/corax89/chip8-test-rom/blob/master/test_opcode.ch8")
+        romUrl: toDownloadUrl("https://github.com/corax89/chip8-test-rom/blob/master/test_opcode.ch8"),
+        platform: "chip8",
+        options: {}
     }
 
-    return j
+    return roms
 }
 
 function toDownloadUrl(url: string) {
