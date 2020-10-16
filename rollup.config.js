da81bd653f085b351194e38730776c6525c6629d
Maik Riechert <maik.riechert@arcor.de>
2020-10-16 22:34:02 +0100

    fix

--- rollup.config.js
+++ rollup.config.js
@@ -3,16 +3,21 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import serve from 'rollup-plugin-serve';
 
-export default {
-  input: "src/host/main.ts",
-  output: {
-    file: "build/bundle.js",
-    format: "iife",
-  },
-  plugins: [
+export default () => {
+  const plugins = [
     nodeResolve(),
     typescript(),
-    commonjs(),
-    serve()
+    commonjs()
   ]
-};
+  if (process.env.SERVE) {
+    plugins.push(serve())
+  }
+  return {
+    input: "src/host/main.ts",
+    output: {
+      file: "build/bundle.js",
+      format: "iife",
+    },
+    plugins: plugins
+  }
+} 
